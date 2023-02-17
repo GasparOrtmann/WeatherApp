51f04174a2bad5256fad41506a6468719380f3af
Gaspar Ortmann <gasparortmann@gmail.com>
2023-02-17 10:58:48 -0300

    Seteo de Datos y hide action bar

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.TextView
@@ -18,9 +20,14 @@ class MainActivity : AppCompatActivity() {
     val API: String = "d727bedd57f1f14c086424e985fbf263"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // HIDE ACTION BAR
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_main)
 
-//        weatherTask.execute()
+        weatherTask().execute()
     }
     inner class weatherTask() : AsyncTask<String, Void, String>(){
         override fun onPreExecute() {
@@ -63,17 +70,21 @@ class MainActivity : AppCompatActivity() {
                 val address = jsonObj.getString("name")+", "+sys.getString("country")
 
                 findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.updated_at).text = address
-                findViewById<TextView>(R.id.status).text = address
-                findViewById<TextView>(R.id.temp).text = address
-                findViewById<TextView>(R.id.temp_max).text = address
-                findViewById<TextView>(R.id.temp_min).text = address
-                findViewById<TextView>(R.id.sunrise).text = address
-                findViewById<TextView>(R.id.address).text = address
-                findViewById<TextView>(R.id.address).text = address
+                findViewById<TextView>(R.id.updated_at).text = updatedAtText
+                findViewById<TextView>(R.id.status).text = weatherDescription.capitalize()
+                findViewById<TextView>(R.id.temp).text = temp
+                findViewById<TextView>(R.id.temp_max).text = tempMax
+                findViewById<TextView>(R.id.temp_min).text = tempMin
+                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
+                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.wind).text = windSpeed
 
-            }catch (e:java.lang.Exception){
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<RelativeLayout>(R.id.mainContainer).visibility = View.VISIBLE
 
+            }catch (e:Exception){
+                findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
+                findViewById<TextView>(R.id.errortext).visibility = View.VISIBLE
             }
         }
     }
